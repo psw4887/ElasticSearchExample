9169c369016685d93cccdc14a74ac78ba9f7bb57
psw4887 <psw4887@naver.com>
2022-07-21 00:17:32 +0900

    Controller Return Setting

--- src/main/java/com/copark/elasticsearchexample/controller/TestController.java
+++ src/main/java/com/copark/elasticsearchexample/controller/TestController.java
@@ -3,10 +3,14 @@ package com.copark.elasticsearchexample.controller;
 import com.copark.elasticsearchexample.entity.jpa.Student;
 import com.copark.elasticsearchexample.service.AcademyService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.net.URI;
 import java.util.List;
 
 @RestController
@@ -17,9 +21,13 @@ public class TestController {
     private final AcademyService academyService;
 
     // TODO 7: 응답 객체를 반환하는 Rest Controller 작성
+    @GetMapping("/students")
     public ResponseEntity<List<Student>> retrieveStudents() {
 
-        return null;
+        return ResponseEntity.status(HttpStatus.OK)
+                .location(URI.create("/elastic/students"))
+                .contentType(MediaType.APPLICATION_JSON)
+                .build();
     }
 
 }
